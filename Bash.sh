da38cb960c2b41c06dc9bf7828cc570d41cbe518
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-01-20 03:43:08 +0100

    Update Bash.sh

--- Bash.sh
+++ Bash.sh
@@ -3,3 +3,8 @@ sccache --show-stats
 cargo build
 sccache --show-stats
 apt-get install build-essential autoconf libssl-dev libyaml-dev zlib1g-dev libffi-dev libgmp-dev rustc
+gem install google-apis-drive_v3 google-apis-calendar_v3
+bundle install
+gem build google-apis-drive_v3.gemspec
+gem install ./google-apis-drive_v3-0.x.x.gem
+ruby -e "require 'google/apis/drive_v3'; require 'google/apis/calendar_v3'; puts 'Success!'"
