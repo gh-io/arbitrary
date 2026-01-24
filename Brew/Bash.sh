818f1f3b6a5bedfcbc35cb8b73d65fc3aa63cbd7
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-01-24 19:12:29 +0100

    Update and rename install.sh to Bash.sh

--- Brew/Bash.sh
+++ Brew/Bash.sh
@@ -1,3 +1,5 @@
 brew install rust
 ruby-build 4.1.0/opt/rubies/ruby-3.2.2 -- --enable-yjit
 apt-get install build-essential autoconf libssl-dev libyaml-dev zlib1g-dev libffi-dev libgmp-dev rustc
+git clone https://github.com/KaTeX/KaTeX.git
+cd KaTeX
